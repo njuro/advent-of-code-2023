a4b50463f7cae814707911ce5411f58ba829d958
Juraj Noge <juraj.noge@productboard.com>
2023-12-03 10:08:49 +0100

    Day 2

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -1,14 +1,29 @@
 import utils.readInputLines
+import kotlin.math.max
 
 /** [https://adventofcode.com/2023/day/2] */
-class Day2 : AdventOfCodeTask {
+class Cubes : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val input = readInputLines("2.txt")
-
-        return -1
+        return readInputLines("2.txt").sumOf { line ->
+            val (game, rounds) = line.split(": ")
+            val cubes = mutableMapOf<String, Int>().withDefault { 0 }
+            rounds.split("; ").forEach { round ->
+                round.split(", ").forEach { hint ->
+                    val (amount, color) = hint.split(" ")
+                    cubes[color] = max(amount.toInt(), cubes.getValue(color))
+                }
+            }
+            if (part2) {
+                cubes.values.reduce { a, b -> a * b }
+            } else {
+                val maxCubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
+                val gameId = game.split(" ").last().toInt()
+                gameId.takeIf { maxCubes.all { (color, amount) -> amount >= cubes.getValue(color) } } ?: 0
+            }
+        }
     }
 }
 
 fun main() {
-    print(Day2().run(part2 = false))
+    print(Cubes().run(part2 = true))
 }
--- src/test/kotlin/AdventOfCodeTasksTest.kt
+++ src/test/kotlin/AdventOfCodeTasksTest.kt
@@ -8,6 +8,11 @@ class AdventOfCodeTasksTest {
         runTaskTest(Digits(), 54450, 54265)
     }
 
+    @Test
+    fun day02() {
+        runTaskTest(Cubes(), 2795, 75561)
+    }
+
     private fun runTaskTest(task: AdventOfCodeTask, part1Result: Any, part2Result: Any) {
         assertEquals(part1Result, task.run())
         assertEquals(part2Result, task.run(part2 = true))
