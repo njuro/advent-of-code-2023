746d7a30672d42379f1a44cd1764b5b75f72e1bf
Juraj Noge <juraj.noge@productboard.com>
2023-12-03 09:51:02 +0100

    Day 1

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,14 +1,21 @@
 import utils.readInputLines
 
 /** [https://adventofcode.com/2023/day/1] */
-class Day1 : AdventOfCodeTask {
+class Digits : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val input = readInputLines("1.txt")
+        val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").withIndex()
+            .associate { it.value to it.index + 1 }
 
-        return -1
+        val pattern = Regex("(?=(${if (part2) digits.keys.joinToString("|") + "|" else ""}\\d))")
+
+        fun MatchResult.toDigit(): Int = groupValues.last().run {
+            if (first().isDigit()) first().digitToInt() else digits.getValue(this)
+        }
+
+        return readInputLines("1.txt").sumOf { pattern.findAll(it).run { first().toDigit() * 10 + last().toDigit() } }
     }
 }
 
 fun main() {
-    print(Day1().run(part2 = false))
+    print(Digits().run(part2 = true))
 }
