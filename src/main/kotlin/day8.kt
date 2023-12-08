4d011cf0eba07d3a27ea1ee074688647986387ea
Juraj Noge <juraj.noge@productboard.com>
2023-12-08 10:35:35 +0100

    Day 8 - part 1

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -1,11 +1,25 @@
-import utils.readInputLines
+import utils.readInputBlock
 
 /** [https://adventofcode.com/2023/day/8] */
 class Day8 : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val input = readInputLines("8.txt")
+        val (instructions, nodes) = readInputBlock("8.txt").split("\n\n")
+        val pattern = Regex("(\\w+) = \\((\\w+), (\\w+)\\)")
+        val map = nodes.lines().filter(String::isNotBlank).associate {
+            val (node, left, right) = pattern.matchEntire(it)!!.destructured
+            node to (left to right)
+        }
 
-        return -1
+        var steps = 0
+        var current = "AAA"
+        while (current != "ZZZ") {
+            val instruction = instructions[steps % instructions.length]
+            val next = map.getValue(current)
+            current = if (instruction == 'L') next.first else next.second
+            steps += 1
+        }
+
+        return steps
     }
 }
 
